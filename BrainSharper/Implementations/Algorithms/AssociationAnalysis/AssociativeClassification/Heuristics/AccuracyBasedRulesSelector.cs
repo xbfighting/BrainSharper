6cbc0ac41825d140dfe66661fead321ac8dcc3e0
Filip Wójcik <filip.wojcik@outlook.com>
2016-01-03 11:31:00 +0100

    Implemented additional heuristic for classification apriori - purerly based on the accuracy

---
+++ BrainSharper/Implementations/Algorithms/AssociationAnalysis/AssociativeClassification/Heuristics/AccuracyBasedRulesSelector.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrainSharper.Implementations.Algorithms.AssociationAnalysis.AssociativeClassification.Heuristics
+{
+    using BrainSharper.Abstract.Algorithms.AssociationAnalysis.AssociativeClassification;
+    using BrainSharper.Abstract.Algorithms.AssociationAnalysis.DataStructures;
+    using BrainSharper.Abstract.Data;
+
+    public class AccuracyBasedRulesSelector<TValue> : ClassificationAprioriRulesSelector<TValue>
+    {
+        public override IAssociativeClassificationModel<TValue> BuildPredictiveRulesSet(
+            IDataFrame dataFrame,
+            ITransactionsSet<IDataItem<TValue>> transactionsSet,
+            string dependentFeatureName,
+            IList<IAssociationRule<IDataItem<TValue>>> associationRules)
+        {
+            var sortedRules =
+                associationRules
+                    .Cast<IClassificationAssociationRule<TValue>>()
+                    .OrderByDescending(rule => rule.Confidence)
+                    .ThenByDescending(rule => rule.RelativeSuppot)
+                    .ThenBy(rule => rule.Antecedent.ItemsSet.Count)
+                    .ToList();
+
+            var rulesCoveringData = new Dictionary<int, Dictionary<int, int>>();
+            var accuracies = new Dictionary<int, double>();
+
+            for (var ruleIdx = 0; ruleIdx < sortedRules.Count; ruleIdx++)
+            {
+                var currentRule = sortedRules[ruleIdx];
+                var ruleCoverage = new Dictionary<int, int>();
+                var coversAtLeastOneExample = false;
+                foreach (var rowIdx in Enumerable.Range(0, dataFrame.RowCount))
+                {
+                    var currentRow = dataFrame.GetRowVector<TValue>(rowIdx);
+                    if (currentRule.Covers(currentRow))
+                    {
+                        coversAtLeastOneExample = true;
+                        var expectedVal = currentRow[dependentFeatureName];
+                        var predictedVal = currentRule.ClassificationConsequent.FeatureValue;
+                        var isCorrect = expectedVal.Equals(predictedVal) ? 1 : 0;
+                        ruleCoverage.Add(rowIdx, isCorrect);
+                    }
+                }
+                if (coversAtLeastOneExample)
+                {
+                    rulesCoveringData.Add(ruleIdx, ruleCoverage);
+                    accuracies.Add(ruleIdx, ruleCoverage.Values.Sum() / (double)ruleCoverage.Count);
+                }
+            }
+
+            var bestRules = new List<int>();
+            while (rulesCoveringData.Any())
+            {
+                var maxAccRule = (from ruleIdxAndAcc in accuracies
+                                 where ruleIdxAndAcc.Value == accuracies.Values.Max()
+                                 select ruleIdxAndAcc.Key).First();
+                var bestRuleCoveredCases = rulesCoveringData[maxAccRule];
+
+                bestRules.Add(maxAccRule);
+                accuracies.Remove(maxAccRule);
+                var emptyRules = new List<int>();
+                foreach (var kvp in rulesCoveringData.Where(el => el.Key != maxAccRule))
+                {
+                    var coveredCases = kvp.Value;
+                    foreach (var bestRuleCase in bestRuleCoveredCases)
+                    {
+                        var caseIdx = bestRuleCase.Key;
+                        if (coveredCases.ContainsKey(caseIdx))
+                        {
+                            coveredCases.Remove(caseIdx);
+                        }
+                    }
+                    if (!coveredCases.Any())
+                    {
+                        emptyRules.Add(kvp.Key);
+                    }
+                }
+
+                foreach (var emptyRule in emptyRules)
+                {
+                    rulesCoveringData.Remove(emptyRule);
+                    accuracies.Remove(emptyRule);
+                }
+
+                rulesCoveringData.Remove(maxAccRule);
+                foreach (var kvp in rulesCoveringData)
+                {
+                    var covering = kvp.Value;
+
+                    accuracies[kvp.Key] = covering.Values.Sum() / (double)covering.Values.Count;
+                }
+            }
+
+            var defaultClass =
+                            dataFrame
+                            .GetColumnVector<TValue>(dependentFeatureName)
+                            .GroupBy(val => val)
+                            .OrderByDescending(grp => grp.Count())
+                            .First()
+                            .Key;
+            return new AssociativeClassificationModel<TValue>(
+                bestRules.Select(idx => sortedRules[idx]).ToList(),
+                defaultClass,
+                dependentFeatureName
+                );
+        }
+    }
+}
--- BrainSharperTests/Implementations/Algorithms/AssociationAnalysis/AssociativeClassification/ClassificationAprioriTest.cs
+++ BrainSharperTests/Implementations/Algorithms/AssociationAnalysis/AssociativeClassification/ClassificationAprioriTest.cs
@@ -23,12 +23,63 @@ namespace BrainSharperTests.Implementations.Algorithms.AssociationAnalysis.Assoc
         private readonly ICrossValidator<string> _splitter = new CrossValidator<string>();
 
         private readonly ClassificationAprioriRulesSelector<string> _basicHeuristic = new BasicCARRulesSelector<string>();
+        private readonly ClassificationAprioriRulesSelector<string> _accHeuristic = new AccuracyBasedRulesSelector<string>();
+
+        [Test]
+        public void BuildAssociativeModelTest_HeuristicsComparison()
+        {
+            // Given
+            var modelBuilder1 = new ClassificationApriori<string>(AssociationMiningParamsInterpreter.AreMinimalRequirementsMet, _basicHeuristic);
+            var modelBuilder2= new ClassificationApriori<string>(AssociationMiningParamsInterpreter.AreMinimalRequirementsMet, _accHeuristic);
+            var testData = TestDataBuilder.ReadIrisDiscretizedData();
+            var miningParams = new ClassificationAssociationMiningParams(
+                TestDataBuilder.DiscretizedIrisDependentFeatureName,
+                0.05,
+                null,
+                0.8);
+
+            // When
+            var model1 = modelBuilder1.BuildModel(testData, "iris", miningParams) as IAssociativeClassificationModel<string>;
+            var model2 = modelBuilder2.BuildModel(testData, "iris", miningParams) as IAssociativeClassificationModel<string>;
+
+            // Then
+            CollectionAssert.AreNotEquivalent(model1.ClassificationRules, model2.ClassificationRules);
+        }
+
 
         [Test]
         public void BuildAssociativeModelTest_BasicCARHeuristic()
         {
             // Given
             var modelBuilder = new ClassificationApriori<string>(AssociationMiningParamsInterpreter.AreMinimalRequirementsMet, _basicHeuristic);
+            var testData = TestDataBuilder.ReadIrisDiscretizedData();
+            var miningParams = new ClassificationAssociationMiningParams(
+                TestDataBuilder.DiscretizedIrisDependentFeatureName,
+                0.05,
+                null,
+                0.8);
+
+            // When
+            var accuracies = _splitter.CrossValidate(
+                modelBuilder,
+                miningParams,
+                _classifier,
+                new ConfusionMatrixBuilder<string>(),
+                testData,
+                "iris",
+                0.7,
+                10);
+
+            // Then
+            var avgAccuracy = accuracies.Select(report => report.Accuracy).First();
+            Assert.GreaterOrEqual(avgAccuracy, 0.9);
+        }
+
+        [Test]
+        public void BuildAssociativeModelTest_AccHeuristic()
+        {
+            // Given
+            var modelBuilder = new ClassificationApriori<string>(AssociationMiningParamsInterpreter.AreMinimalRequirementsMet, _accHeuristic);
             var testData = TestDataBuilder.ReadIrisDiscretizedData();
             var miningParams = new ClassificationAssociationMiningParams(
                 TestDataBuilder.DiscretizedIrisDependentFeatureName,
